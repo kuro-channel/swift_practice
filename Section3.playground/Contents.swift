ee0373f1a2f0fb6c1e3ebead4de04eee24db75b4
kuroiwa-t <t.kuroiwa.0216@gmail.com>
2021-12-02 11:26:11 +0900

    20211202 swift practice

---
+++ Section3.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// 変数
+var x = 10
+
+// 定数
+let y = 10
+//y = 20
+
+let strName = 19
+
+// Swiftでは、CamelCase推奨
+let priceOfApple = 100
+let price_of_apple = 100
+
+// 例題：1冊1000円の本を10冊購入した時の合計金額
+let priceOfBook = 1000
+let buyCount = 10
+let totalBookPrice = priceOfBook * buyCount
