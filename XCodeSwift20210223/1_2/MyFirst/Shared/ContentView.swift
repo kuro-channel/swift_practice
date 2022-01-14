f53c9836756277470eff788a27a66a1d419ae3dd
kuroiwa-t <t.kuroiwa.0216@gmail.com>
2022-01-14 23:47:22 +0900

    20220114

---
+++ XCodeSwift20210223/1_2/MyFirst/Shared/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 黒岩卓誠 on 2022/01/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+//        Text("Hello, world!")
+        Text("Hi, Swift!")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
